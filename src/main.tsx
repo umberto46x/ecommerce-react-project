11df5e2b065b81297bc9c650e2745c0e1e89f8a7
umberto46x <daniele.u.nicosia.dev@gmail.com>
2025-07-22 21:30:25 +0200

    files , added favicon,cart and logo SVGs

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles/index.css';
+import App from './App.tsx';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/router.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <RouterProvider router={router}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </RouterProvider>
+);
